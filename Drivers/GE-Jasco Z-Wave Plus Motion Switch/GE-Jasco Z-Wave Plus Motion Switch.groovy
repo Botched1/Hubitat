f27bd15095cb99d67b06757aade900b8e73032b4
Botched1 <jason@bottjen.net>
2019-02-28 19:37:18 -0600

    Create GE-Jasco Z-Wave Plus Motion Switch.groovy

---
+++ Drivers/GE-Jasco Z-Wave Plus Motion Switch/GE-Jasco Z-Wave Plus Motion Switch.groovy
@@ -0,0 +1,318 @@
+/**
+ *  IMPORT URL: https://raw.githubusercontent.com/Botched1/Hubitat/master/Drivers/GoControl%20GC-TBZ48/GoControl%20GC-TBZ48.groovy
+ *
+ *  GE-Jasco Z-Wave Plus Motion Switch
+ *
+ *  Original custom DTH Author: Matt Lebaugh (@mlebaugh)
+ *
+ *  HUBITAT PORT
+ *  1.0.0 (02/28/2019) - Original version.
+ */
+
+metadata {
+	definition (name: "GE Z-Wave Plus Motion Switch", namespace: "Botched1", author: "Jason Bottjen") {
+		capability "Actuator"
+		capability "Motion Sensor"
+		capability "PushableButton"
+		capability "Configuration"
+		capability "Refresh"
+		capability "Sensor"
+		capability "Switch"		
+		capability "Light"
+	}
+
+ preferences {
+     input (type: "paragraph", element: "paragraph", title: "Switch General Settings", description: "")
+	 input "paramLightTimer", "enum", title: "Light Timeout", description: "Length of time after no motion for the light to shut off in Occupancy/Vacancy modes", options: ["0" : "5 seconds", "1" : "1 minute", "5" : "5 minutes (default)", "15" : "15 minutes", "30" : "30 minutes", "255" : "disabled"], required: false, displayDuringSetup: true
+	 input "paramOperationMode", "enum", title: "Operating Mode", description: "Occupancy: Automatically turn on and off the light with motion\nVacancy: Manually turn on, automatically turn off light with no motion.", options: ["1" : "Manual", "2" : "Vacancy", "3" : "Occupancy (default)"], required: false, displayDuringSetup: true
+	 input "paramInverted", "enum", title: "Switch Buttons Direction", multiple: false, options: ["0" : "Normal (default)", "1" : "Inverted"], required: false, displayDuringSetup: true
+	 input "paramMotionEnabled", "enum", title: "Motion Sensor", description: "Enable/Disable Motion Sensor.",options: ["0" : "Disable","1" : "Enable (default)"], required: false
+	 input "paramMotionSensitivity", "enum", title: "Motion Sensitivity", description: "Motion Sensitivity", options: ["1" : "High", "2" : "Medium (default)", "3" : "Low"], required: false, displayDuringSetup: true
+	 input "paramLightSense", "enum", title: "Light Sensing", description: "If enabled, Occupancy mode will only turn light on if it is dark", options: ["0" : "Disabled","1" : "Enabled (default)"], required: false, displayDuringSetup: true
+	 input "paramMotionResetTimer", "enum", title: "Motion Detection Reset Time", options: ["0" : "Disabled", "1" : "10 sec", "2" : "20 sec (default)", "3" : "30 sec", "4" : "45 sec", "110" : "27 mins"], required: false
+	 //
+	 input ( type: "paragraph", element: "paragraph", title: "", description: "Logging")
+	 input name: "logEnable", type: "bool", title: "Enable debug logging", defaultValue: true		
+    }
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Parse
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+def parse(String description) {
+    def result = null
+	if (description != "updated") {
+		if (logEnable) log.debug "parse() >> zwave.parse($description)"
+		def cmd = zwave.parse(description, [0x20: 1, 0x25: 1, 0x56: 1, 0x70: 2, 0x72: 2, 0x85: 2])
+
+		if (logEnable) log.debug "cmd: $cmd"
+		
+		if (cmd) {
+			result = zwaveEvent(cmd)
+        }
+	}
+    if (!result) { log.warn "Parse returned ${result} for $description" }
+    else {if (logEnable) log.debug "Parse returned ${result}"}
+	
+	return result
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Z-Wave Messages
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+def zwaveEvent(hubitat.zwave.commands.crc16encapv1.Crc16Encap cmd) {
+	log.warn("zwaveEvent(): CRC-16 Encapsulation Command received: ${cmd}")
+	def encapsulatedCommand = zwave.getCommand(cmd.commandClass, cmd.command, cmd.data,1)
+	if (encapsulatedCommand) {
+       zwaveEvent(encapsulatedCommand)
+   } else {
+       log.warn "Unable to extract CRC16 command from ${cmd}"
+   }
+}
+
+def zwaveEvent(hubitat.zwave.commands.basicv1.BasicReport cmd) {
+    log.debug "---BASIC REPORT V1--- ${device.displayName} sent ${cmd}"
+	//createEvent(name: "switch", value: cmd.value ? "on" : "off", isStateChange: true)
+}
+
+def zwaveEvent(hubitat.zwave.commands.basicv1.BasicSet cmd) {
+	
+    if (logEnable) log.debug "---BASIC SET V1--- ${device.displayName} sent ${cmd}"
+	def result = []
+	
+    return result
+}
+
+def zwaveEvent(hubitat.zwave.commands.associationv2.AssociationReport cmd) {
+	if (logEnable) log.debug "---ASSOCIATION REPORT V2--- ${device.displayName} sent groupingIdentifier: ${cmd.groupingIdentifier} maxNodesSupported: ${cmd.maxNodesSupported} nodeId: ${cmd.nodeId} reportsToFollow: ${cmd.reportsToFollow}"
+    if (cmd.groupingIdentifier == 3) {
+    	if (cmd.nodeId.contains(zwaveHubNodeId)) {
+        	sendEvent(name: "numberOfButtons", value: 2, displayed: false)
+        }
+        else {
+        	sendEvent(name: "numberOfButtons", value: 0, displayed: false)
+			zwave.associationV2.associationSet(groupingIdentifier: 3, nodeId: zwaveHubNodeId).format()
+			zwave.associationV2.associationGet(groupingIdentifier: 3).format()
+        }
+    }
+}
+
+def zwaveEvent(hubitat.zwave.commands.configurationv2.ConfigurationReport cmd) {
+	if (logEnable) log.debug "---CONFIGURATION REPORT V2--- ${device.displayName} sent ${cmd}"
+    def config = cmd.scaledConfigurationValue.toInteger()
+    def result = []
+	def name = ""
+    def value = ""
+    def reportValue = config // cmd.configurationValue[0]
+    switch (cmd.parameterNumber) {
+        case 1:
+            name = "Light Timeout"
+            value = reportValue == 0 ? "5 seconds" : reportValue == 1 ? "1 minute" : reportValue == 5 ? "5 minutes (default)" : reportValue == 15 ? "15 minutes" : reportValue == 30 ? "30 minutes" : reportValue == 255 ? "disabled" : "error"
+            break
+        case 3:
+            name = "Operating Mode"
+            value = reportValue == 1 ? "Manual" : reportValue == 2 ? "Vacancy" : reportValue == 3 ? "Occupancy (default)": "error"
+            break
+        case 5:
+            name = "Invert Buttons"
+            value = reportValue == 0 ? "Disabled (default)" : reportValue == 1 ? "Enabled" : "error"
+            break
+        case 6:
+            name = "Motion Sensor"
+            value = reportValue == 0 ? "Disabled" : reportValue == 1 ? "Enabled (default)" : "error"
+            break
+        case 13:
+            name = "Motion Sensitivity"
+            value = reportValue == 1 ? "High" : reportValue == 2 ? "Medium (default)" :  reportValue == 3 ? "Low" : "error"
+            break
+        case 14:
+            name = "Light Sensing"
+            value = reportValue == 0 ? "Disabled" : reportValue == 1 ? "Enabled (default)" : "error"
+            break
+        case 15:
+			//log.debug "Motion Reset Timer is " + reportValue + ". Config is " + config
+            name = "Motion Reset Timer"
+            value = reportValue == 0 ? "Disabled" : reportValue == 1 ? "10 seconds" : reportValue == 2 ? "20 seconds (default)" : reportValue == 3 ? "30 seconds" : reportValue == 4 ? "45 seconds" : reportValue == 110 ? "27 minutes" : "error"
+            break
+        default:
+            break
+    }
+	result << createEvent([name: name, value: value, displayed: false])
+	return result
+}
+
+def zwaveEvent(hubitat.zwave.commands.switchbinaryv1.SwitchBinaryReport cmd) {
+    if (logEnable) log.debug "---BINARY SWITCH REPORT V1--- ${device.displayName} sent ${cmd}"
+    
+	def desc
+	
+	if (cmd.value == 255) {
+		desc = "Switch turned ON"
+	}
+	else if (cmd.value == 0) {
+		desc = "Switch turned OFF"	
+	}
+	createEvent([name: "switch", value: cmd.value ? "on" : "off", descriptionText: "$desc", isStateChange: true])
+}
+
+def zwaveEvent(hubitat.zwave.commands.manufacturerspecificv2.ManufacturerSpecificReport cmd) {
+    log.debug "---MANUFACTURER SPECIFIC REPORT V2--- ${device.displayName} sent ${cmd}"
+	log.debug "manufacturerId:   ${cmd.manufacturerId}"
+	log.debug "manufacturerName: ${cmd.manufacturerName}"
+    state.manufacturer=cmd.manufacturerName
+	log.debug "productId:        ${cmd.productId}"
+	log.debug "productTypeId:    ${cmd.productTypeId}"
+	def msr = String.format("%04X-%04X-%04X", cmd.manufacturerId, cmd.productTypeId, cmd.productId)
+	updateDataValue("MSR", msr)	
+    sendEvent([descriptionText: "$device.displayName MSR: $msr", isStateChange: false])
+}
+
+def zwaveEvent(hubitat.zwave.commands.versionv1.VersionReport cmd) {
+	def fw = "${cmd.applicationVersion}.${cmd.applicationSubVersion}"
+	updateDataValue("fw", fw)
+	if (logEnable) log.debug "---VERSION REPORT V1--- ${device.displayName} is running firmware version: $fw, Z-Wave version: ${cmd.zWaveProtocolVersion}.${cmd.zWaveProtocolSubVersion}"
+}
+
+def zwaveEvent(hubitat.zwave.commands.hailv1.Hail cmd) {
+	log.warn "Hail command received..."
+	[name: "hail", value: "hail", descriptionText: "Switch button was pressed", displayed: false]
+}
+
+def zwaveEvent(hubitat.zwave.Command cmd) {
+    log.warn "${device.displayName} received unhandled command: ${cmd}"
+}
+
+def zwaveEvent(hubitat.zwave.commands.notificationv3.NotificationReport cmd)
+{
+	if (logEnable) log.debug "---NOTIFICATION REPORT V3--- ${device.displayName} sent ${cmd}"
+	def result = []
+	if (cmd.notificationType == 0x07) {
+		if ((cmd.event == 0x00)) { 
+			result << createEvent(name: "motion", value: "inactive", descriptionText: "$device.displayName motion has stopped", isStateChange: true)
+		} else if (cmd.event == 0x08) {
+			result << createEvent(name: "motion", value: "active", descriptionText: "$device.displayName detected motion", isStateChange: true)
+		} 
+	} 
+	result
+}
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Driver Commands / Functions
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+def on() {
+	if (logEnable) log.debug "Turn device ON"
+	delayBetween([
+		zwave.switchBinaryV1.switchBinarySet(switchValue: 0xFF).format(),
+		zwave.switchBinaryV1.switchBinaryGet().format()
+	],500)
+}
+
+def off() {
+	if (logEnable) log.debug "Turn device OFF"
+	delayBetween([
+		zwave.switchBinaryV1.switchBinarySet(switchValue: 0x00).format(),
+		zwave.switchBinaryV1.switchBinaryGet().format()
+	],500)
+}
+
+def refresh() {
+	log.info "refresh() is called"
+	
+	def cmds = []
+	cmds << zwave.switchBinaryV1.switchBinaryGet().format()
+	cmds << zwave.notificationV3.notificationGet(notificationType: 7).format()
+    cmds << zwave.configurationV2.configurationGet(parameterNumber: 1).format()
+    cmds << zwave.configurationV2.configurationGet(parameterNumber: 3).format()
+    cmds << zwave.configurationV2.configurationGet(parameterNumber: 5).format()
+    cmds << zwave.configurationV2.configurationGet(parameterNumber: 6).format()
+    cmds << zwave.configurationV2.configurationGet(parameterNumber: 13).format()
+    cmds << zwave.configurationV2.configurationGet(parameterNumber: 14).format()
+    cmds << zwave.configurationV2.configurationGet(parameterNumber: 15).format()
+	if (getDataValue("MSR") == null) {
+		cmds << zwave.manufacturerSpecificV1.manufacturerSpecificGet().format()
+	}
+	delayBetween(cmds,1000)
+}
+
+def installed() {
+	configure()
+}
+
+def updated() {
+    log.info "updated..."
+    log.warn "debug logging is: ${logEnable == true}"
+    log.warn "description logging is: ${txtEnable == true}"
+    if (logEnable) runIn(1800,logsOff)
+
+    if (state.lastUpdated && now() <= state.lastUpdated + 3000) return
+    state.lastUpdated = now()
+
+	def cmds = []
+    cmds << zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:zwaveHubNodeId).format()
+	cmds << zwave.associationV1.associationRemove(groupingIdentifier:2, nodeId:zwaveHubNodeId).format()
+	cmds << zwave.associationV1.associationRemove(groupingIdentifier:3, nodeId:zwaveHubNodeId).format()
+
+	// Set Light Timer param
+	if (paramLightTimer==null) {
+		paramLightTimer = 5
+	}
+	cmds << zwave.configurationV2.configurationSet(scaledConfigurationValue: paramLightTimer, parameterNumber: 1, size: 1).format()
+	cmds << zwave.configurationV2.configurationGet(parameterNumber: 1).format()
+	
+	
+	// Set Operation Mode param
+	if (paramOperationMode==null) {
+		paramOperationMode = 3
+	}
+	cmds << zwave.configurationV2.configurationSet(scaledConfigurationValue: paramOperationMode, parameterNumber: 3, size: 1).format()
+	cmds << zwave.configurationV2.configurationGet(parameterNumber: 3).format()
+	
+	// Set Inverted param
+	if (paramInverted==null) {
+		paramInverted = 0
+	}
+	cmds << zwave.configurationV2.configurationSet(scaledConfigurationValue: paramInverted, parameterNumber: 5, size: 1).format()
+	cmds << zwave.configurationV2.configurationGet(parameterNumber: 5).format()
+
+	// Set Motion Enabled param
+	if (paramMotionEnabled==null) {
+		paramMotionEnabled = 1
+	}
+	cmds << zwave.configurationV2.configurationSet(scaledConfigurationValue: paramMotionEnabled, parameterNumber: 6, size: 1).format()
+	cmds << zwave.configurationV2.configurationGet(parameterNumber: 6).format()
+
+	// Set Motion Sensitivity param
+	if (paramMotionSensitivity==null) {
+		paramMotionSensitivity = 2
+	}
+	cmds << zwave.configurationV2.configurationSet(scaledConfigurationValue: paramMotionSensitivity, parameterNumber: 13, size: 1).format()
+	cmds << zwave.configurationV2.configurationGet(parameterNumber: 13).format()
+
+	// Set Light Sense param
+	if (paramLightSense==null) {
+		paramLightSense = 1
+	}
+	cmds << zwave.configurationV2.configurationSet(scaledConfigurationValue: paramLightSense, parameterNumber: 14, size: 1).format()
+	cmds << zwave.configurationV2.configurationGet(parameterNumber: 14).format()
+
+	// Set Motion Reset Timer param
+	if (paramMotionResetTimer==null) {
+		paramMotionResetTimer = 2
+	}
+	cmds << zwave.configurationV2.configurationSet(scaledConfigurationValue: paramMotionResetTimer, parameterNumber: 15, size: 1).format()
+	cmds << zwave.configurationV2.configurationGet(parameterNumber: 15).format()
+
+	//
+	delayBetween(cmds, 500)
+}
+
+def configure() {
+        log.info "configure() called"
+		def cmds = []
+        cmds << zwave.associationV1.associationSet(groupingIdentifier:1, nodeId:zwaveHubNodeId).format()
+		cmds << zwave.associationV1.associationRemove(groupingIdentifier:2, nodeId:zwaveHubNodeId).format()
+		cmds << zwave.associationV1.associationSet(groupingIdentifier:3, nodeId:zwaveHubNodeId).format()
+        delayBetween(cmds, 500)
+}
+
+def logsOff(){
+    log.warn "debug logging disabled..."
+    device.updateSetting("logEnable",[value:"false",type:"bool"])
+}
