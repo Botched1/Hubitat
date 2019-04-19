ad6a1739423a6296170360d0bbc8d3a89ee78d27
Botched1 <jason@bottjen.net>
2019-04-19 13:04:34 -0500

    Update Sony TV Lite.groovy

--- Drivers/Sony TV Lite/Sony TV Lite.groovy
+++ Drivers/Sony TV Lite/Sony TV Lite.groovy
@@ -1,7 +1,7 @@
 /**
  *  Sony TV Lite
  *  Lite version for Hubitat by Jason Bottjen, original version info and credits below
- *  
+ *  IMPORT URL: https://raw.githubusercontent.com/Botched1/Hubitat/master/Drivers/Sony%20TV%20Lite/Sony%20TV%20Lite.groovy
  *
  *  Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
  *  in compliance with the License. You may obtain a copy of the License at:
