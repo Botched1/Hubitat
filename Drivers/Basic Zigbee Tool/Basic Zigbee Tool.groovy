4087f785eeed129ecd543cb22b669829cedf68b5
Botched1 <jason@bottjen.net>
2019-12-04 11:24:11 -0600

    Create Basic Zigbee Tool.groovy

---
+++ Drivers/Basic Zigbee Tool/Basic Zigbee Tool.groovy
@@ -0,0 +1,37 @@
+/**
+ *  Author: Jason Bottjen
+ *  Date: 2019-12-04
+ */
+
+metadata {
+	definition (name: "Basic Zigbee Tool", namespace: "botched1", author: "Jason Bottjen") {
+        
+        command "test"
+
+    }
+}
+    
+
+def parse(String description) {
+    logDebug "Parse description $description"
+    def descMap = zigbee.parseDescriptionAsMap(description)
+    logDebug "Desc Map: $descMap"
+	  def map = [:]
+    def result = null
+	  if (map) {
+		  result = createEvent(map)
+	  }
+  	logDebug "Parse returned $map"
+	  return result
+}
+
+def test() {
+    logDebug "test"
+    def cmds = []
+    
+    cmds += zigbee.readAttribute(0x001, 0x0020) //Read Battery Voltage
+    
+    //    cmds += zigbee.configureReporting(0x201, 0x0000, 0x29, 10, 60, 50)   //Attribute ID 0x0000 = local temperature, Data Type: S16BIT
+    
+    return cmds
+}
