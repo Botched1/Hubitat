c57444868a6cf21613338fe119f9e969bb70f31f
Botched1 <jason@bottjen.net>
2020-08-26 10:33:00 -0500

    Update Motion Dimmer Parent.groovy

--- Drivers/Dev/Motion Dimmer Parent.groovy
+++ Drivers/Dev/Motion Dimmer Parent.groovy
@@ -21,15 +21,7 @@
 
 metadata {
 	definition (name: "GE Z-Wave Plus Motion Dimmer Parent", namespace: "Botched1", author: "Jason Bottjen") {
-		//capability "Actuator"
-		//capability "Motion Sensor"
-		//capability "PushableButton"
 		capability "Configuration"
-		//capability "Refresh"
-		//capability "Sensor"
-		//capability "Switch"
-		//capability "Switch Level"
-		//capability "Light"
 		
 		command "setDefaultDimmerLevel", [[name:"Default Dimmer Level",type:"NUMBER", description:"Default Dimmer Level Used when Turning ON. (0=Last Dimmer Value)", range: "0..99"]]
 		command "setLightTimeout", [[name:"Light Timeout",type:"ENUM", description:"Time before light turns OFF on no motion - only applies in Occupancy and Vacancy modes.", constraints: ["5 seconds", "1 minute", "5 minutes (default)", "15 minutes", "30 minutes", "disabled"]]]
