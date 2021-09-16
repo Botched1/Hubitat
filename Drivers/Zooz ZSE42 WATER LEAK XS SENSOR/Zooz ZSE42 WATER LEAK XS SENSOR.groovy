ea7102702d86e03ed977f31362431376a4c14adc
Botched1 <jason@bottjen.net>
2021-09-16 16:59:17 -0500

    Update Zooz ZSE42 WATER LEAK XS SENSOR.groovy

--- Drivers/Zooz ZSE42 WATER LEAK XS SENSOR/Zooz ZSE42 WATER LEAK XS SENSOR.groovy
+++ Drivers/Zooz ZSE42 WATER LEAK XS SENSOR/Zooz ZSE42 WATER LEAK XS SENSOR.groovy
@@ -6,6 +6,7 @@
  *  1.0.0 (08/16/2021) - Initial Version
  *  1.0.1 (08/21/2021) - Removed redundant battery report on wakeup
  *  1.1.0 (09/07/2021) - Added debug logging command and firmware version retrieval on parameter update
+ *  1.1.1 (09/16/2021) - Fixed bug that qould throw an error if LED parameter wasn't set in preferences.
  */
 
 import groovy.transform.Field
@@ -222,7 +223,7 @@ def updateConfig() {
 	
 	// Set LED param
 	if (paramLEDIndicatorMode==null) {
-		paramLED = 1
+		paramLEDIndicatorMode = 1
 	}
 	cmds.add(zwave.configurationV2.configurationSet(scaledConfigurationValue: paramLEDIndicatorMode.toInteger(), parameterNumber: 1, size: 1).format())
 	cmds.add(zwave.configurationV2.configurationGet(parameterNumber: 1).format())
